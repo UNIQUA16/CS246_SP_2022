fcc17802cc68aa2fa12155ad38fb1dd88216c1e8
UNIQUA ROBINSON <urobinson@mec.science>
2022-02-01 17:02:24 -0500

    bool function

---
+++ Labs/Starter.h
@@ -0,0 +1,28 @@
+#include <iostream>;
+using namespace std;
+#include <array>;
+
+
+
+bool IsBinaryString('p')
+{
+    if ('p' == '0' || 'p' ='1') {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+    
+}
+
+int AbsoluteSum()
+
+
+
+int main () {
+    IsBinaryString(02);
+    return 0;
+   
+   
+}
